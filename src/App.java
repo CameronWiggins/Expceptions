1bef39500744d818662da0334cd7bf3ca0620c8a
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-09-11 10:24:22 -0500

    exceptions wokring

--- src/App.java
+++ src/App.java
@@ -4,12 +4,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter a Number to find its factors. (or enter exit to leave the program)");
+        System.out.println("Enter a Number to find its Factors. (or Enter exit to leave the Program)");
         String userInput = scan.nextLine();
 
         while(!(userInput.equals("exit")))
         {
+            try 
+            {
+                Integer.parseInt(userInput);
+            }
+
+            catch(NumberFormatException error)
+            {
+                System.out.println("Invalid Input " + error.getMessage());
+            }
+
             int num = Integer.parseInt(userInput);
+
+            System.out.println("Enter a Number to find its Factors. (or Enter exit to leave the Program)");
+            userInput = scan.nextLine();
         }
 
         
