fd20cb267af00f0e6f8a9ab48e36fb5b22104c0f
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-09-11 10:02:59 -0500

    added loop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a Number to find its factors. (or enter exit to leave the program)");
+        String userInput = scan.nextLine();
+
+        while(!(userInput.equals("exit")))
+        {
+            int num = Integer.parseInt(userInput);
+        }
+
+        
     }
 }
+
+
